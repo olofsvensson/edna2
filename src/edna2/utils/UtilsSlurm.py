558a276d0b89fae1b1d9190790c83c8156e9e30f
Olof Svensson <svensson@esrf.fr>
2023-12-15 15:30:45 +0100

    Code moved from bes.workflow_lib.mxnice

--- src/edna2/utils/UtilsSlurm.py
+++ src/edna2/utils/UtilsSlurm.py
@@ -23,7 +23,7 @@ __authors__ = ["O. Svensson"]
 __license__ = "MIT"
 __date__ = "13/10/2023"
 
-
+import os
 import shlex
 import subprocess
 import threading
@@ -31,11 +31,9 @@ import threading
 
 def run_command_line(command_line, timeout_sec=120):
     proc = subprocess.Popen(
-        shlex.split(command_line),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
+        shlex.split(command_line), stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
-    kill_proc = lambda p: p.kill()
+    kill_proc = lambda p: p.kill()  # noqa E731
     timer = threading.Timer(timeout_sec, kill_proc, [proc])
     try:
         timer.start()
@@ -82,3 +80,93 @@ def scancel(job_id):
     scancel_command_line = f"scancel {job_id}"
     stdout, stderr = run_command_line(scancel_command_line, timeout_sec)
 
+
+def submit_job_to_slurm(
+    command_line,
+    working_directory,
+    nodes=1,
+    core=4,
+    time="2:00:00",
+    host=None,
+    queue=None,
+    name=None,
+    mem=None,
+):
+    slurm_id = None
+    script = "#!/bin/bash\n"
+    if name is not None:
+        script += '#SBATCH --job-name="{0}"\n'.format(name)
+    if queue is None:
+        partition = "mx"
+    else:
+        partition = "mx,{0}".format(queue)
+    script += "#SBATCH --partition={0}\n".format(partition)
+    if mem is None:
+        mem = 4000  # 4 Gb memory by default
+    script += "#SBATCH --mem={0}\n".format(mem)
+    script += "#SBATCH --ntasks={0}\n".format(nodes)
+    script += "#SBATCH --nodes=1\n"  # Necessary for not splitting jobs! See ATF-57
+    script += "#SBATCH --cpus-per-task={0}\n".format(core)
+    script += "#SBATCH --time={0}\n".format(time)
+    script += "#SBATCH --output={0}/stdout.txt\n".format(working_directory)
+    script += "#SBATCH --error={0}/stderr.txt\n".format(working_directory)
+    script += command_line + "\n"
+    script_name = "slurm.sh"
+    slurm_script_path = os.path.join(working_directory, script_name)
+    with open(slurm_script_path, "w") as f:
+        f.write(script)
+    stdout, stderr = run_command_line("sbatch {0}".format(slurm_script_path))
+    if "Submitted batch job" in stdout:
+        slurm_id = int(stdout.split("job")[1])
+    return slurm_script_path, slurm_id, stdout, stderr
+
+
+def split_at_equal(line, dict_line={}):
+    if "=" in line:
+        key, value = line.split("=", 1)
+        if "=" in value:
+            if " " in value:
+                part1, part2 = value.split(" ", 1)
+            elif "," in value:
+                part1, part2 = value.split(",", 1)
+            dict_line[key.strip()] = part1.strip()
+            dict_line = split_at_equal(part2, dict_line=dict_line)
+        else:
+            dict_line[key.strip()] = value.strip()
+    return dict_line
+
+
+def parse_slurm_stat(slurmStat):
+    list_lines = slurmStat.split("\n")
+    dict_slurm_stat = {}
+    for line in list_lines:
+        if line != "":
+            dictLine = split_at_equal(line)
+            dict_slurm_stat.update(dictLine)
+    return dict_slurm_stat
+
+
+def get_slurm_stat(slurm_job_id):
+    dict_stat = None
+    stdout, stderr = run_command_line("scontrol show job {0}".format(slurm_job_id))
+    if stderr is None or stderr == "":
+        dict_stat = parse_slurm_stat(stdout)
+    return dict_stat
+
+
+def are_jobs_pending(partition_name=None):
+    jobs_are_pending = False
+    if partition_name is not None:
+        stdout, stderr = run_command_line(
+            "squeue -p {0} -t PENDING".format(partition_name)
+        )
+    else:
+        stdout, stderr = run_command_line("squeue -t PENDING")
+    list_lines = stdout.split("\n")
+    no_pending = 0
+    for line in list_lines:
+        if "Resources" in line or "Priority" in line:
+            no_pending += 1
+    if no_pending > 2:
+        jobs_are_pending = True
+    return jobs_are_pending
